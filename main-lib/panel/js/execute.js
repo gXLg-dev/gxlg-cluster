a64bf4e83de4f6cfecf6057facf17131e40b54e6
gXLg <natgaev@gmail.com>
2025-01-25 23:47:35 +0100

    fix hanging command

--- main-lib/panel/js/execute.js
+++ main-lib/panel/js/execute.js
@@ -4,11 +4,11 @@ const state = require("./terminal.js");
 module.exports = (req, res) => {
   if (!req.auth) return res.redirect("/");
   const input = req.body.input + "; echo; pwd -P";
-  const command = spawnSync("sh", ["-c", input], { "cwd": state.cwd });
-  const lines = command.stdout.toString().trim().split("\n");
-  state.cwd = lines.pop();
+  const command = spawnSync("sh", ["-c", input], { "cwd": state.cwd, "encoding": "utf-8", "timeout": 10000 });
+  const lines = command.stdout.trim().split("\n");
+  state.cwd = lines.pop() ?? state.cwd;
   const o = lines.join("\n").trim();
-  const e = command.stderr.toString().trim();
+  const e = command.stderr.trim();
   state.output = [state.cwd, o, e].join("\n---\n");
   res.redirect("/tools");
 };
