6d6c5a5674f89eba4e062b31ef13917a6e09a7db
gXLg <natgaev@gmail.com>
2025-02-15 14:20:54 +0100

    fix undici; update panel shutdown

--- main-lib/panel.js
+++ main-lib/panel.js
@@ -2,17 +2,18 @@ const { nulls } = require("nulls");
 const { check } = require("./panel/js/auth.js");
 
 let server;
-nulls({
-  "nulls": "main-lib/panel/html",
-  "uploads": false,
-  "static": "main-lib/panel/static",
-  "init": (_, s) => { server = s; },
-  "forceHttps": true,
-  "hook": (req, res) => {
-    const token = req.cookies["token.cluster"];
-    req.auth = check(token);
-  }
-});
+(async () => {
+  server = await nulls({
+    "nulls": "main-lib/panel/html",
+    "uploads": false,
+    "static": "main-lib/panel/static",
+    "forceHttps": true,
+    "hook": (req, res) => {
+      const token = req.cookies["token.cluster"];
+      req.auth = check(token);
+    }
+  });
+})();
 
 function close() {
   server.close();
--- main-lib/panel/js/execute.js
+++ main-lib/panel/js/execute.js
@@ -5,10 +5,10 @@ module.exports = (req, res) => {
   if (!req.auth) return res.redirect("/");
   const input = req.body.input + "; echo; pwd -P";
   const command = spawnSync("sh", ["-c", input], { "cwd": state.cwd, "encoding": "utf-8", "timeout": 10000 });
-  const lines = command.stdout.trim().split("\n");
+  const lines = (command.stdout ?? "").trim().split("\n");
   state.cwd = lines.pop() ?? state.cwd;
   const o = lines.join("\n").trim();
-  const e = command.stderr.trim();
+  const e = (command.stderr ?? "").trim();
   state.output = [state.cwd, o, e].join("\n---\n");
   res.redirect("/tools");
 };
